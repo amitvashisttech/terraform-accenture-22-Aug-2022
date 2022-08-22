ce6fd6364b9a1b4c0ac4e6bdae1699fde325486d
Amit Vashist <amitvashist7@outlook.com>
2022-08-22 09:30:27 +0000

    02-First-Deployment

--- 03-CLI-Common-Exec/main.tf
+++ 03-CLI-Common-Exec/main.tf
@@ -3,36 +3,13 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "web" { 
-  ami = "ami-0cea098ed2ac54925"
+resource "aws_instance" "web" {
+  ami           = "ami-0cea098ed2ac54925"
   instance_type = "t2.micro"
-  
- tags = { 
-   Name = "WebServer-AV"
-   Team = "DevOps"
-  } 
 
-}
-
-resource "aws_instance" "db" { 
-  ami = "ami-0cea098ed2ac54925"
-  instance_type = "t2.micro"
-  
- tags = {
-   Name = "WebServer-AV"
-   Team = "DevOps"
+  tags = {
+    Name = "WebServer-AV"
+    Team = "DevOps"
   }
-
-}
-
-
-resource "aws_instance" "web1" { 
-  ami = "ami-0cea098ed2ac54925"
-  instance_type = "t2.micro"
-  
- tags = { 
-   Name = "WebServer-AV"
-   Team = "DevOps"
-  } 
 
 }
